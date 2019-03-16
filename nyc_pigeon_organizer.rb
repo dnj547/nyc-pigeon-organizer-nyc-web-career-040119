2b84fe53eb09f22773da0b71894256190defa21e
Danielle Jasper <dnj547@live.com>
2019-03-16 22:52:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,8 +5,10 @@ def nyc_pigeon_organizer(data)
     attribute_hash.each do |attribute, birds_array|
       birds_array.each do |bird|
         if new_hash[bird].nil?
-          new_hash[bird] = {attribute_key => attribute}
-          binding.pry
+          attribute_array = [attribute.to_s]
+          new_hash[bird] = {attribute_key => attribute_array}
+        else
+          new_hash[bird][attribute_key] << attribute.to_s
         end
       end
     end
