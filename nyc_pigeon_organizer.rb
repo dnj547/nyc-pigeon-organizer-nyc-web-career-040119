8042bcef7a404b8135efa7e5a89255c8bdd15ffd
Danielle Jasper <dnj547@live.com>
2019-03-16 22:46:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  
+  new_hash = {}
+  data.each do |attribute_key, attribute_hash|
+    attribute_hash.each do |attribute, birds_array|
+      birds_array.each do |bird|
+        if new_hash[bird].nil?
+          new_hash[bird] = attribute_key
+          new_hash[bird][attribute_key] = attribute
+        end
+      end
+    end
+  end
 end
