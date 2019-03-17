53fddbacab666fc7b6bd008f9292e3d44fa50339
Danielle Jasper <dnj547@live.com>
2019-03-17 00:52:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,8 +13,8 @@ def nyc_pigeon_organizer(data)
           attribute_array << attribute.to_s
           new_hash[bird] = {key => attribute_array}
         end
+        binding.pry
       end
-      binding.pry
     end
   end
   new_hash
